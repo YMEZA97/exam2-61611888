351684e6ef4752b7b00ba44f4c17d8bff758acc8
YMEZA97 <yasalberto@hotmail.com>
2024-09-16 19:49:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flutter_exam2_61611888
+
+A new Flutter project.
+# exam2-61611888
---
+++ android/app/src/main/kotlin/com/example/flutter_exam2_61611888/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_exam2_61611888
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
